c6cfeccb16b47abb9feb3ab230467bb9dcdfc0f8
alaiayf <a.yeregui@orekait.com>
2025-05-07 13:08:13 +0200

    primero

---
+++ .next - copia/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4/B1yDt4fDE5zU0etiWBeDsEiUSuTtb5xitv3sHSTgg=\"\n}"
---
+++ .next - copia/static/chunks/app_page_tsx_47de8a._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_47de8a._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_47de8a._.js",
+  "chunks": [
+    "static/chunks/[next]_internal_font_google_323c5c._.css",
+    "static/chunks/a7abb_next_dist_0e6813._.js",
+    "static/chunks/app_page_tsx_9fdd4f._.js"
+  ],
+  "source": "dynamic"
+});
